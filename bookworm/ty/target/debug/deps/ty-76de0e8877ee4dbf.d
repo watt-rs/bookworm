f4fad8857f7458cf2eb5bcc94b7a847cca60bff6
vyacheslavhere <slavlendstudio@gmail.com>
2026-02-11 18:12:10 +0500

    remove artifacts. add .gitignore

--- bookworm/ty/target/debug/deps/ty-76de0e8877ee4dbf.d
+++
@@ -1,5 +0,0 @@
-/home/vyacheslav/bookworm/crates/ty/target/debug/deps/ty-76de0e8877ee4dbf.d: src/lib.rs
-
-/home/vyacheslav/bookworm/crates/ty/target/debug/deps/libty-76de0e8877ee4dbf.rmeta: src/lib.rs
-
-src/lib.rs:
