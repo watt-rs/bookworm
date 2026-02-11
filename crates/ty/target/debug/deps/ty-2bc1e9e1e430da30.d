ef065c406c0d187ae2d46f9caf854ce775bc5475
vyacheslavhere <slavlendstudio@gmail.com>
2026-02-11 18:04:58 +0500

    remove target, create .gitignore

--- crates/ty/target/debug/deps/ty-2bc1e9e1e430da30.d
+++
@@ -1,5 +0,0 @@
-/home/vyacheslav/bookworm/crates/ty/target/debug/deps/ty-2bc1e9e1e430da30.d: src/lib.rs
-
-/home/vyacheslav/bookworm/crates/ty/target/debug/deps/libty-2bc1e9e1e430da30.rmeta: src/lib.rs
-
-src/lib.rs:
